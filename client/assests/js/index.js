b9c9efce92db86517774a914ac97dac22cf63c16
nahimanajz <jazzonahimana@gmail.com>
2022-05-25 22:56:51 +0200

    show data from weatherapp hosted apis

--- client/assests/js/index.js
+++ client/assests/js/index.js
@@ -1,48 +1,60 @@
 window.addEventListener('DOMContentLoaded', () => {
 
  console.log('content loaded successfully');
-const index_url = `http://localhost:2000/api/v1/regions/`
+const index_url = `https://blooming-inlet-03785.herokuapp.com/api/v1/regions/`
  getWeathers(index_url)
 })
+const newTag=(tagName)=> document.createElement(tagName);
+const getTag=(parentElementClass,childTag)=> document.getElementsByClassName(parentElementClass)[0].appendChild(childTag)
 
 const getWeathers = async (url) => {
  const  weathers = await fetch(url).then(response => response.json()).then(response => response.weathers)
- console.log(weathers)
  populateToDiv(weathers)
 }
 
 const populateToDiv = (weathers) => {
    const weathersList = document.querySelector('.grid')
-   const children = []
     weathers.map(({_id, region, degrees, measure, updatedAt}) => {
-        let deleteBtn = document.createElement('span');
+        let deleteBtn = newTag('span');
         deleteBtn.innerText = 'X'
         deleteBtn.addEventListener('click', (e) => {
             alert(_id)
             
         })
-        
-        const handleDelete =(id) => {
-            console.log(id)
-             //TODO: call API To delete record then refresh
-         }
-        const div = `<div class="child">
-        <div class="flex flex-row-btn">
-            <span>Edit </span>
-            <span onclick="handleDelete(${_id})" id="close" class="close"> <i class="fa-solid fa-xmark"></i> X </span>
-        </div>
-        <div class="pd-20">
-            <h1 id="title"> ${region} </h1>
-            <p class="conf-text" contenteditable="true">
-                Degrees: ${degrees}<sup>0</sup>${measure === 'celcius'? `C`:`F`}<br />
-                Last updated on: ${updatedAt}
-            </p>
-            <button id="btn-continue" onclick="${handleDelete(_id)}"> Continue</button>
-        </div>
-       </div>`
-       
-       children.push(div)
+        // weathersList.appendChild(deleteBtn)
+        let child = newTag("div")       
+        let flex = newTag("div")  
+        let div2 = newTag("div")     
+        let span1 = newTag("span")
+        span1.innerText = 'Edit'
+        let span2 = newTag("span")
+        child.classList.add("child")
+        span2.innerText = 'X'
+
+        span2.addEventListener('click', ()=> alert(_id))  
+        flex.classList.add("flex")     
+        flex.classList.add("flex-row-btn")
+        flex.appendChild(span1)
+        flex.appendChild(span2)
+        div2.innerHTML= ` <div class="pd-20">
+        <h1 id="title"> ${region} </h1>
+        <p class="conf-text" contenteditable="true">
+            Degrees: ${degrees}<sup>0</sup>${measure === 'celcius'? `C`:`F`}<br />
+            Last updated on: ${updatedAt}
+        </p>
+        <button id="btn-continue" onclick="${handleDelete(_id)}"> Continue</button>
+    </div>`
+         
+        child.appendChild(flex)
+        child.appendChild(div2)
+        weathersList.appendChild(child)
+
     })
-    weathersList.innerHTML = children.join('')
+    
 }
 
+
+const handleDelete =(id) => {
+    console.log(id)
+     //TODO: call API To delete record then refresh
+ }
