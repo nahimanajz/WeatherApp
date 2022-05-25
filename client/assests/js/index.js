3e077c29472492e53a39c5c1e5cf9e7c729d0d88
nahimanajz <jazzonahimana@gmail.com>
2022-05-25 23:23:41 +0200

    Delete document

--- client/assests/js/index.js
+++ client/assests/js/index.js
@@ -1,7 +1,8 @@
+const index_url = `https://blooming-inlet-03785.herokuapp.com/api/v1/regions/`
+
 window.addEventListener('DOMContentLoaded', () => {
 
  console.log('content loaded successfully');
-const index_url = `https://blooming-inlet-03785.herokuapp.com/api/v1/regions/`
  getWeathers(index_url)
 })
 const newTag=(tagName)=> document.createElement(tagName);
@@ -14,37 +15,39 @@ const getWeathers = async (url) => {
 
 const populateToDiv = (weathers) => {
    const weathersList = document.querySelector('.grid')
-    weathers.map(({_id, region, degrees, measure, updatedAt}) => {
+    weathers.map(({_id, region, degrees, measure, updatedAt}, index) => {
         let deleteBtn = newTag('span');
         deleteBtn.innerText = 'X'
         deleteBtn.addEventListener('click', (e) => {
             alert(_id)
             
         })
-        // weathersList.appendChild(deleteBtn)
         let child = newTag("div")       
         let flex = newTag("div")  
         let div2 = newTag("div")     
         let span1 = newTag("span")
         span1.innerText = 'Edit'
+        span1.addEventListener('click', () => localStorage.setItem('weather', JSON.stringify(weathers[index])))  
+
         let span2 = newTag("span")
         child.classList.add("child")
         span2.innerText = 'X'
 
-        span2.addEventListener('click', ()=> alert(_id))  
+        span2.addEventListener('click', () => handleDelete(_id))  
+
         flex.classList.add("flex")     
         flex.classList.add("flex-row-btn")
         flex.appendChild(span1)
         flex.appendChild(span2)
+
         div2.innerHTML= ` <div class="pd-20">
         <h1 id="title"> ${region} </h1>
         <p class="conf-text" contenteditable="true">
             Degrees: ${degrees}<sup>0</sup>${measure === 'celcius'? `C`:`F`}<br />
             Last updated on: ${updatedAt}
         </p>
-        <button id="btn-continue" onclick="${handleDelete(_id)}"> Continue</button>
     </div>`
-         
+
         child.appendChild(flex)
         child.appendChild(div2)
         weathersList.appendChild(child)
@@ -53,8 +56,17 @@ const populateToDiv = (weathers) => {
     
 }
 
-
-const handleDelete =(id) => {
+const handleDelete = (id) => {
     console.log(id)
-     //TODO: call API To delete record then refresh
+    fetch(`${index_url}id`, {
+        method: 'DELETE',
+      })
+      .then(res => res.text()) // or res.json()
+      .then(res => console.log(res))
+      //TODO: Add functionality to display alert
  }
+const saveToUpdate =(weather) => {
+ console.log(weather)
+ localStorage.setItem('weather', weather)
+}
+
