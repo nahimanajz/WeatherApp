cb5fe3e8bb155d7cd0d6db8c36fce52e545ab044
nahimanajz <jazzonahimana@gmail.com>
2022-05-25 08:56:54 +0200

    (ft-weather-store): Save weather info

--- server/controller/weather.js
+++ server/controller/weather.js
@@ -1,71 +1,17 @@
 import Validate from "../util/Validator";
-import db from '../config/DBConnector';
+import WeatherModal from '../model/weather';
 
 class Weather {
-    async store(req, res) {
-        console.log(req.body)
-        const { error } = Validate.weatherInfo(req.body);
-        if (error) {
-            console.log(JSON.stringify(error));
-            return res.send({
-                status: 400,
-                error: error.details[0].message
-            });
-        }
-        try {
-            const isRegionExist = await dbc.query('SELECT * FROM weathers WHERE region=$1', [req.body.region]);
-            if (isRegionExist.rowCount > 0) {
-                return res.send({
-                    status:403,
-                    message: 'Region must be recorded only once'
-                });
-            }
-            const query = "INSERT INTO weathers(region, degree, measure) VALUES($1, $2, $3, )";
-            const { region, degree, measure } = req.body
-            const values = [region, degree, measure]
-            const weather = await dbc.query(query, values)
-            return weath && res.send({ status: 201, message: 'New weather is recorded', weather })
-        } catch (error) {
-            return res.status(400).send({
-                status: 400,
-                error: error,
-
-            });
-        }
-
-    }
-    async getOne(req, res) {
-        const weather = await db.query('SELECT * FROM weathers WHERE region=$1', [req.body.region])
-        return res.status(200).send({weather})
-
-    }
-    async getAll(req, res) {
-        try {
-            const weathers = await dbc.query('SELECT * FROM weathers');
-            return res.status(200).send({weathers})
-
-        } catch (error) {
-            return res.status(500).json({
-                status: 500,
-                error: error
-            });
-
-        }
-
-    }
-    async delete(req, res) {
-        try {
-            const weather = await dbc.query('DELETE FROM weathers WHERE id=$1', [req.params.id]);
-            return res.status(200).json({
-                status: 200,
-                data: 'specified region is deleted'
-            });
-        } catch (error) {
-            return res.status(500).json({
-                status: 500,
-                error: error
-            });
-        }
+    async store(req, res){
+        const { region, degrees, measure } = req.body
+        const newModal = new WeatherModal({region, degrees, measure });
+        const savedRes = await newModal.save().then(resp=>{
+            return res
+                .send({
+                message:'new response is saved',
+                data: resp
+            })
+        }).catch(error=> res.send(error))
 
     }
 }
--- server/model/weather.js
+++ server/model/weather.js
@@ -1,6 +1,21 @@
-class Weather {
-    async getRegion(region) {
-        const rows  = await db.executeQuery('SELECT * FROM weathers WHERE region = $1', [region]);
-        return rows[0]
+import mongoose from 'mongoose';
+
+const weatherSchema = new mongoose.Schema({
+    degrees:{
+        type: Number,
+        required:true
+    },
+    region:{
+        type: String,
+         required:true,
+         unique:true
+        },
+    measure:{
+        type: String,
+        required:false
     }
-}
+}, {
+    timestamps: true
+});
+ const WeatherModal = mongoose.model('Weathers', weatherSchema)
+ export default WeatherModal ;
